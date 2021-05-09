0dc4bf3352dd2312cd9a4bf75f40c6b6f03a6ae8
leon <ishimweleon@gmail.com>
2021-05-09 16:37:05 +0300

    50% working

--- game.py
+++ game.py
@@ -1,19 +1,40 @@
 import utils
 
+print(utils.getDest(0, 0, 1, "y"))
+
 board = utils.getBoard()
-print(board["data"][6:12])
+utils.printTable(board)
+utils.move(board, "2a5", "x"), utils.printTable(board)
+utils.move(board, "2l1", "y"), utils.printTable(board)
+utils.move(board, "6j1", "y"), utils.printTable(board)
+utils.printTable(board)
+typea = "x"
 
-for i in range(3):
-    print(i)
-else:
-    print("nah")
-
-# while True:
-#     print("Rolling...")
-#     rolls = utils.rollDice()
-#     print(f"Rolled {rolls[0]} and {rolls[1]}")
-#     rolls = rolls + rolls if rolls[0] == rolls[1] else rolls
-#     while len(rolls) != 0:
-#         sources = input(f"dice = {rolls[-1]}, Please enter source :")
-#         s1 = utils._toIndex(sources)
-#         rolls.pop()
+while True:
+    typea = "y" if typea == "x" else "x"
+    print("Rolling...")
+    # rolls = utils.rollDice()
+    rolls = [1,1]
+    board["data"][2][5]["str"] = str(rolls[0])
+    board["data"][2][6]["str"] = str(rolls[1])
+    print(f"Rolled {rolls[0]} and {rolls[1]}")
+    rolls = rolls + rolls if rolls[0] == rolls[1] else rolls
+    while len(rolls) != 0:
+        print("rolls", rolls)
+        utils.printTable(board)
+        if utils.flanksVar(board, typea):
+            if not utils.canFlankBeMoved(board, rolls, typea):
+                print("Can not be moved, going to next player")
+                break
+            utils.moveFlank(board, rolls, typea)
+            continue
+        if not utils.movesVar(board, rolls, typea):
+            print("No moves available, next player")
+            break
+        else:
+            source = input("Please choose source: ")
+            dest = utils.parse(source)
+            if not utils.move(board, source, typea):
+                print("TRy again, you can do it")
+            else:
+                rolls.remove(dest[0])
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import random
 import json
+import functools
 
 
 def rollDice():
@@ -7,24 +8,28 @@ def rollDice():
 
 
 def getBoard():
-    data = [{"count": 0, "type": "", 'str': ""} for i in range(6+6+6+6)]
-    data[0] = {"count": 5, "type": "y", 'str': "5y"}
-    data[4] = {"count": 3, "type": "x", 'str': "3x"}
+    data = [[{"count": 0, "type": None, 'str': ""} for i in range(12)] for i in range(5)]
+    data[0][0] = {"count": 5, "type": "y", 'str': "5y"}
+    data[0][4] = {"count": 3, "type": "x", 'str': "3x"}
 
-    data[6] = {"count": 5, "type": "x", 'str': "5x"}
-    data[11] = {"count": 2, "type": "y", 'str': "2y"}
+    data[0][6] = {"count": 5, "type": "x", 'str': "5x"}
+    # data[0][11] = {"count": 2, "type": "y", 'str': "2y"}
 
-    data[12] = {"count": 2, "type": "x", 'str': "2x"}
-    data[17] = {"count": 5, "type": "y", 'str': "5y"}
+    data[4][11] = {"count": 2, "type": "x", 'str': "2x"}
+    # data[4][6] = {"count": 5, "type": "y", 'str': "5y"}
 
-    data[19] = {"count": 3, "type": "y", 'str': "3y"}
-    data[23] = {"count": 5, "type": "x", 'str': "5x"}
+    # data[4][4] = {"count": 3, "type": "y", 'str': "3y"}
+    data[4][0] = {"count": 5, "type": "x", 'str': "5x"}
+
+    data[2][4] = {"count": 0, "type": "y", 'str': "0x"} #xflanks
+    data[2][7] = {"count": 0, "type": "y", 'str': "0y"} #yflanks
+
+    data[2][5] = {"count": 0, "type": "y", 'str': "0"} #dice1
+    data[2][6] = {"count": 0, "type": "y", 'str': "0"} #dice2
 
 
     board = {
         "data": data,
-        "x_flank": 0,
-        "y_flank": 0,
         "turn": "x",
         "x": 15,
         "y": 15,
@@ -38,17 +43,10 @@ def printTable(board):
     for i in "ABCDEFGHIJKL":
         print(f"{i:<5}", end='')
     print()
-    for i in range(12):
-        cell = board['data'][i]
-        print(f"{board['data'][i]['str']:<5}", end = '')
-    print()
-
-    for i in range(23, 11, -1):
-        print(f"{board['data'][i]['str']:<5}", end = '')
-    print()
-    # for i in "ABCDEFGHIJKL":
-        # print(f"{i:<5}", end='')
-    # print()
+    for row in board["data"]:
+        for cell in row:
+            print(f"{cell['str']:<5}", end="")
+        print()
 
 def save(board):
     fp = open("dump.json", "w")
@@ -60,28 +58,42 @@ def restore():
     board = json.load(fp)
     return board
 
-def toIndex(turns):
-    turn1, turn2 = turns.split(" ")
-    if turn1[1:] != turn2[1:]:
+#3e3 becomes (3, 2, 4) (dice roll, rowNo, colNo)
+def parse(source):
+    parsed = ord(source[1]) - ord('a')
+    return int(source[0]), int(source[2]) - 1, parsed
+
+
+def getDest(row, col, num, typea = ""):
+    dest = None
+    if row == 0 and col - num < 0 and typea == "y":
+        dest = [4, 11 - (col- num)%12]
+
+    elif row == 4 and col - num < 0 and typea == "x":
+        dest = [0, 11 - (col- num)%12]
+    elif row == 0:
+        dest = [row, col + (num if typea=="x" else -num)]
+    else:
+        dest = [row, col - (num if typea=="x" else -num)]
+    
+    if dest[1]<0 or dest[1] >= 12:
+        print("invalid range", dest)
         return None
-    turn1 = _toIndex(turn1)
-    turn2 = _toIndex(turn2)
-    # print(turn1, turn2)
-    return turn1, turn2
+    return dest
 
-def _toIndex(turn):
-    if turn[1:] == '1':
-        index = ord(turn[0]) - 97
-        return index if 0 <= index < 12 else None
 
-    index = 122 - ord(turn[0]) - 2
-    return index if 23 >= index >= 12 else None
+def move(board, source, typea = "x"):
+    parsed = parse(source)
+    row = parsed[1]
+    col = parsed[2]
+    num = parsed[0]
+    dest = getDest(row, col, num, typea)
 
-def move(board, source, num, typea = "x"):
-    num = num if typea=="x" else -num
+    if dest is None:
+        return False
 
-    fromCell = board["data"][source]
-    toCell = board["data"][(source + num) % 24]
+    fromCell = board["data"][row][col]
+    toCell = board["data"][dest[0]][dest[1]]
     if fromCell["count"] == 0 or fromCell["type"] != typea:
         print(f"0 {typea} found here")
         return False
@@ -97,16 +109,21 @@ def move(board, source, num, typea = "x"):
             toCell["count"] = 1
             toCell["type"] = "x"
             toCell["str"] = "1x"
-            board["y_flank"] += 1
+            board["data"][2][7]["count"] += 1
+            board["data"][2][7]["str"] = f'{board["data"][2][7]["count"]}y'
         else:
             fromCell["count"] -= 1
             fromCell["str"] = f"{fromCell['count']}y"
             toCell["count"] = 1
             toCell["type"] = "y"
             toCell["str"] = "1y"
-            board["x_flank"] += 1
-        board["data"][source] = fromCell
-        board["data"][(source + num) % 24] = toCell
+            board["data"][2][4]["count"] += 1
+            board["data"][2][4]["str"] = f'{board["data"][2][4]["count"]}x'
+        if fromCell["count"] == 0:
+            fromCell["str"] = ""
+            fromCell["type"] = None
+        board["data"][row][col] = fromCell
+        board["data"][dest[0]][dest[1]] = toCell
         print("finished moving")
 
     elif toCell["count"] == 0 or toCell["type"] == typea:
@@ -126,14 +143,86 @@ def move(board, source, num, typea = "x"):
 
         if fromCell["count"] == 0:
             fromCell["str"] = ""
-        if toCell["count"] == 0:
-            toCell["str"] = ""
-        board["data"][source] = fromCell
-        board["data"][(source + num) % 24] = toCell
+            fromCell["type"] = None
+        board["data"][row][col] = fromCell
+        board["data"][dest[0]][dest[1]] = toCell
         print("finished moving last")
     else:
         return False
     return True
 
 
-# toIndex("e2 b2")
+def flanksVar(board, typea):
+    if typea == "x":
+        return board["data"][2][4]["count"] > 0
+    else:
+        return board["data"][2][7]["count"] > 0
+
+def canFlankBeMoved(board, rolls, typea = "x"):
+    cells = []
+    if typea == "x":
+        cells = [board["data"][4][12-i] for i in rolls]
+    else:
+        cells = [board["data"][0][12-i] for i in rolls]
+    for cell in cells:
+        if cell["type"] == typea or cell["count"] <= 1:
+            return True
+    print("Cannot be moved")
+    return False
+
+
+def moveFlank(board, rolls, typea = "x"):
+    dest = input(f"Please choose destination for trapped {typea}")
+    parsed = parse(dest)
+    num = parsed[0]
+    row = parsed[1]
+    col = parsed[2]
+
+    if num not in rolls:
+        print("Not in rolls")
+        return False
+
+    toCell = board["data"][4][12 - num]
+
+    if toCell["count"] ==0 or toCell["type"] == typea:
+        toCell["count"] += 1
+        toCell["type"] = typea
+        toCell["str"] = f"{toCell['count']}{typea}"
+        if typea == "x":
+            board["data"][2][4]["count"] -= 1
+            board["data"][2][4]["str"] = f'{board["data"][2][4]["count"]}x'
+        else:
+            board["data"][2][7] -= 1
+            board["data"][2][7]["str"] = f'{board["data"][2][7]["count"]}x'
+        rolls.remove(num)
+        return True
+    
+    elif toCell["count"] == 1:
+        toCell["count"] = 1
+        toCell["type"] = typea
+        toCell["str"] = f"{toCell['count']}{typea}"
+        if typea == "x":
+            board["data"][2][4] -= 1
+            board["data"][2][7] += 1
+        else:
+            board["data"][2][7] -= 1
+            board["data"][2][4] += 1
+        rolls.remove(num)
+        return True
+
+    else:
+        print("Cant move")
+        return False
+
+def movesVar(board, rolls, typea = "x"):
+    for i in range(12):
+        for ab in [0,4]:
+            if board["data"][ab][i]["type"] == typea and board["data"][ab][i]["count"] >0:
+                for roll in rolls:
+                    dest = getDest(ab, i, roll, typea)
+                    if dest is None:
+                        continue
+                    toCell = board["data"][dest[0]][dest[1]]
+                    if toCell["type"] == typea or toCell["count"] <= 1:
+                        return True
+    return False
